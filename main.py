12caf63009521c4b9a3cecb8726f4d5940a4ba7e
dwipaalfarisi <dwipa.alfarisi@gmail.com>
2022-11-23 09:24:57 +0700

    fix is_product_exists

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
-'''
+"""
 An script based app for self-service cashier service
 Customer can do the following thing:
 1. Add item details:
     - add item name
     - add item quantity
     - add item price
-'''
+"""
+import pandas as pd
+
+def add_user(user_id, user_pass):
+    employee = {user_id: user_pass}
+    return employee
+
+def save_user():
+    _user = add_user()
+    
+
+def read_user():
+    # read json/csv
+
+def login(emp_id, user_pass):
+    # use conditional from read_user 
 
 class Transaction:
-    '''
-    USE DATACLASS??????
-    '''
-    def add_item(item_name: str=None, item_quantity: int=None, item_price: float=None):
-        '''
+    """
+    Class for all transactions
+    """
+    def add_item(
+        item_name: str = None, item_quantity: int = None, item_price: float = None
+    ):
+        """
         Add item:
             item_name
             item_amount
-            item_price 
-        '''
-        flag=True
+            item_price
+        """
+        flag = True
         while flag:
             item_name = str(input("What's the item you want to add?"))
             item_quantity = int(input("How many item you want to add?"))
             item_price = float(input("What's the item price?"))
-            flag=False
+            flag = False
 
         print(item_name, item_quantity, item_price)
 
+def main():
+    #run user validation first
+    # then run order
+
+
 if __name__ == "__main__":
-    Transaction.add_item()
+    # Transaction.add_item()
+    save_user()
